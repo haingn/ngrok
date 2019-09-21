fb4555df6b14910acb0ec19528725d1d881ce0ad
haingn <52133074+haingn@users.noreply.github.com>
2019-09-21 19:26:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,18 +11,22 @@ if [ -z "$1" ]; then
 fi
 
 mkdir -p /opt/ngrok
+
 cd ngrok-setup
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
 unzip ngrok-stable-linux-arm.zip
+
 cp ngrok /opt/ngrok/
 chmod +x /opt/ngrok/ngrok
 cp uninstall.sh /opt/ngrok/
 cp ngrok.service /lib/systemd/system/
 cp ngrok.yml /opt/ngrok/
-rm -rf ~/ngrok-setup/
 sed -i "s/<add_your_token_here>/$1/g" /opt/ngrok/ngrok.yml
+
 systemctl enable ngrok.service
 systemctl start ngrok.service
 systemctl status ngrok.service
-cd ~/
 
+cd ~/
+sleep 3
+rm -rf ~/ngrok-setup/
